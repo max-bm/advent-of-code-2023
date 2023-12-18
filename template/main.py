af30fd0e6b25879cebbc397e3f21be74a4556785
Maxwell Buckmire-Monro <maxbuckmiremonro@gmail.com>
2023-12-18 00:02:47 +0000

    added template

---
+++ template/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def part_one(puzzle_input: List[str]) -> None:
+    ...
+
+
+def part_two(puzzle_input: List[str]) -> None:
+    ...
+
+
+def read_input_file(filename: str) -> List[str]:
+    with open(filename, "r") as f:
+        return [line.rstrip() for line in f]
+
+
+if __name__ == "__main__":
+    test_input = read_input_file("test_input.txt")
+    puzzle_input = read_input_file("input.txt")
+    print(part_one(puzzle_input))
+    print(part_two(puzzle_input))
